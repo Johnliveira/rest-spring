7a3c7dbf0769eab6b97d6ca9031d9eaaba95d007
João Oliveira <jonhjonholiveira@gmail.com>
2023-11-21 20:05:17 -0300

    feat: Upload first steps Spring Boot 3

---
+++ first-steps/src/main/java/com/joaolabs/firststeps/Startup.java
@@ -0,0 +1,13 @@
+package com.joaolabs.firststeps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
